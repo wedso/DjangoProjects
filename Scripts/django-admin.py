9da09c1053b9f16953698cf0f938266639e57e88
Othman Souli <SouliO@psac-afpc.com>
2019-06-25 17:19:53 -0400

    Initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\work\coding\dev\trydjango\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ src/pages/views.py
@@ -0,0 +1,18 @@
+
+
+from django.http import HttpResponse
+from  django.shortcuts import render
+
+# Create your views here.
+
+def home_view(request , *args,**kwargs):
+  #    return HttpResponse("<h1>Hello World</h1>")   # String for HTML Code
+       return render(request, "home.html", {})
+
+def contact_view(request , *args,**kwargs):
+  #    return HttpResponse("<h1>Contact page</h1>")   # String for HTML Code
+    return render(request , "contact.html" , {})
+
+def about_view(request , *args,**kwargs):
+  #    return HttpResponse("<h1>Hello World</h1>")   # String for HTML Code
+       return render(request ,"about.html", {})
